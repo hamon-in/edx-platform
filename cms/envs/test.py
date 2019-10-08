19b83324c1da44034851470c00e77ca668797ffe
syedimranhassan <imran.hassan@arbisoft.com>
2019-10-08 14:28:14 +0500

    Move cms defaults from configration repo

--- cms/envs/test.py
+++ cms/envs/test.py
@@ -141,7 +141,7 @@ if os.environ.get('DISABLE_MIGRATIONS'):
 
 LMS_BASE = "localhost:8000"
 LMS_ROOT_URL = "http://{}".format(LMS_BASE)
-FEATURES['PREVIEW_LMS_BASE'] = "preview.localhost:18000"
+FEATURES['PREVIEW_LMS_BASE'] = "preview.localhost"
 LOGIN_URL = EDX_ROOT_URL + '/signin'
 
 
